c893ba239f517e54955cea6fcb3a781ebcbb4dfa
wlricardo <wlricardo@gmail.com>
2024-05-23 22:09:47 -0300

    Testes finais

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -103,9 +103,13 @@ namespace TrilhaApiDesafio.Controllers
 
             // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
             // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
-            tarefaBanco = tarefa;
+            tarefaBanco.Titulo = tarefa.Titulo;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Status = tarefa.Status;
 
-            return Ok();
+            _context.SaveChanges();
+            return Ok(tarefaBanco);
         }
 
         [HttpDelete("{id}")]
